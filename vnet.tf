b83923c8643d164beca1fe6c42ed66cdc6b2ccff
Muhamad Khilji <muhammadkhilji88@gmail.com>
2021-02-11 20:12:34 +0000

    Added vnet module

--- vnet.tf
+++ vnet.tf
@@ -1,11 +1,11 @@
-resource "azurerm_resource_group" "test" {
-  name     = "my-resources"
-  location = "West Europe"
-}
+// resource "azurerm_resource_group" "test" {
+//   name     = "my-resources"
+//   location = "West Europe"
+// }
 
-  module "network" {
-  source  = "app.terraform.io/KHILJI--training/network/azurerm"
-  version = "3.0.1"
-  resource_group_name = "my-resources"
-  # insert required variables here
-}
+//   module "network" {
+//   source  = "app.terraform.io/KHILJI--training/network/azurerm"
+//   version = "3.0.1"
+//   resource_group_name = "my-resources"
+//   # insert required variables here
+// }
